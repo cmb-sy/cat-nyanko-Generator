21e6d642b4d936eae24c447419a27a4596056ad3
cmb-sy <free.nakashima@gmail.com>
2024-03-28 10:57:28 +0900

    update

---
+++ test/increment3.js
@@ -0,0 +1,5 @@
+function increment_ts(num) {
+    return num + 1;
+}
+// console.log(increment_ts("999"));
+console.log(increment_ts(999));
---
+++ test/increment3.ts
@@ -0,0 +1,5 @@
+function increment_ts(num: number): number {
+  return num + 1;
+}
+// console.log(increment_ts("999"));
+console.log(increment_ts(999));
